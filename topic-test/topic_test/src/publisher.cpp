993bce319ef2ec2dd1111f8bf074f0eece5a9f6e
jkx-ros2 <jkxjkx1031@163.com>
2020-12-22 01:53:43 +0800

    test pub/sub latency

--- topic-test/topic_test/src/publisher.cpp
+++ topic-test/topic_test/src/publisher.cpp
@@ -2,18 +2,20 @@
 #include <string>
 #include <memory>
 #include <functional>
+#include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
+using namespace std::chrono;
 
 class TestPublisher : public rclcpp::Node
 {
 public:
     TestPublisher(): Node("test_publisher")
     {
-        pub_ = create_publisher<std_msgs::msg::String>("topic", 50);
+        pub_ = create_publisher<std_msgs::msg::String>("topic", 10);
         timer_ = create_wall_timer(
             500ms, std::bind(&TestPublisher::timer_callback, this)
         );
@@ -21,11 +23,15 @@ public:
 private:
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
     rclcpp::TimerBase::SharedPtr timer_;
+    int counter_ = 0;
     void timer_callback()
     {
         auto msg = std_msgs::msg::String();
-        msg.data = std::string("[this is a string of length 32]#");
-        RCLCPP_INFO(get_logger(), "Put: %s", msg.data.c_str());
+        msg.data = std::to_string(++counter_);
+        msg.data = std::string(10 - msg.data.size(), ' ') + msg.data;   // align messages
+        // RCLCPP_INFO(get_logger(), "Put: %s", msg.data.c_str());
+        std::cout << counter_ << " " <<
+            duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() << "\n";
         pub_->publish(msg);
     }
 };
--- topic-test/topic_test/src/subscriber.cpp
+++ topic-test/topic_test/src/subscriber.cpp
@@ -1,11 +1,14 @@
+#include <chrono>
 #include <string>
 #include <memory>
 #include <functional>
+#include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
+using namespace std::chrono;
 
 class TestSubscriber : public rclcpp::Node
 {
@@ -13,7 +16,7 @@ public:
     TestSubscriber(): Node("test_subscriber")
     {
         sub_ = create_subscription<std_msgs::msg::String>(
-            "topic", 50,
+            "topic", 10,
             std::bind(&TestSubscriber::topic_callback, this, std::placeholders::_1)
         );
     }
@@ -21,7 +24,10 @@ private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
     void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
     {
-        RCLCPP_INFO(get_logger(), "Get: %s", msg->data.c_str());
+        // RCLCPP_INFO(get_logger(), "Get: %s", msg->data.c_str());
+        int n = std::stoi(msg->data);
+        std::cout << n << " " <<
+            duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() << "\n";
     }
 };
 
