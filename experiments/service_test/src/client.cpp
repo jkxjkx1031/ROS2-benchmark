cb861de7854d2a12ab98e4877fa4175ee4f8f884
jkx-ros2 <jkxjkx1031@163.com>
2021-01-03 14:09:53 +0800

    plot cli/srv throughput

--- experiments/service_test/src/client.cpp
+++ experiments/service_test/src/client.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <memory>
@@ -13,14 +14,17 @@ using namespace std::chrono_literals;
 class Stats
 {
 public:
+    static std::atomic_int message_count;
     static std::vector<long long> rtt[20];
     static void reset()
     {
+        message_count = 0;
         for (int i = 0; i < 20; i++)
             rtt[i].clear();
     }
 };
 
+std::atomic_int Stats::message_count;
 std::vector<long long> Stats::rtt[20];
 
 inline long long get_timestamp_now()
@@ -37,7 +41,7 @@ void run_client(int node_id, int duration)
 
     while (!client->wait_for_service(1s)) /* wait */;
 
-    const int MAX_REQUEST_COUNT = 10000;
+    const int MAX_REQUEST_COUNT = 100000;
     auto request = std::make_shared<example_interfaces::srv::Trigger::Request>();
     long long start = get_timestamp_now();
     for (int i = 1; i <= MAX_REQUEST_COUNT; i++)
@@ -46,7 +50,10 @@ void run_client(int node_id, int duration)
         auto result = client->async_send_request(request);
         rclcpp::spin_until_future_complete(node, result);
         long long receive_ts = get_timestamp_now();
-        Stats::rtt[node_id].push_back(receive_ts - send_ts);
+        if (receive_ts - start > duration * 1e6)
+            break;
+        // Stats::rtt[node_id].push_back(receive_ts - send_ts);
+        Stats::message_count++;
     }
 }
 
@@ -54,7 +61,7 @@ int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     const int DURATION = 1;
-    const int MAX_NODE_COUNT = 1;
+    const int MAX_NODE_COUNT = 16;
     std::vector<std::thread> ClientRunners(MAX_NODE_COUNT);
     for (int n = 1; n <= MAX_NODE_COUNT; n++)
     {
@@ -63,18 +70,7 @@ int main(int argc, char *argv[])
             ClientRunners[i] = std::thread(run_client, i + 1, DURATION);
         for (int i = 0; i < n; i++)
             ClientRunners[i].join();
-        // double avg = 0;
-        // int count = 0;
-        // for (int i = 1; i <= n; i++)
-        // {
-        //     count += Stats::rtt[i].size();
-        //     for (auto x : Stats::rtt[i])
-        //         avg += x;
-        // }
-        // avg /= count;
-        // std::cout << avg << "\n";
-        for (auto x : Stats::rtt[1])
-            std::cout << x << "\n";
+        std::cout << Stats::message_count << "\n";
         std::cerr << "n=" << n << " completed.\n";
     }
     rclcpp::shutdown();
